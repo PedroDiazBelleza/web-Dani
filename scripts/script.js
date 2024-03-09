5131158c0c22bea6cd934c5ca1eca6f78c15ad23
Pedro Diaz Belleza <pedro.diazbelleza@gmail.com>
2024-03-08 23:53:50 -0500

    final

--- scripts/script.js
+++ scripts/script.js
@@ -3,9 +3,11 @@ $horas = document.getElementById('horas'),
 $minutos = document.getElementById('minutos'),
 $segundos = document.getElementById('segundos'),
 $panelFinal = document.querySelector('.panel_final');
-
+$contenedor_historia = document.getElementById('contenedor_historia');
+$espacio_3 = document.querySelector('.espacio_3');
+$body = document.querySelector('body');
 //Fecha final
-const fechaFinal = new Date('Mar 10, 2024 00:00:00').getTime();
+const fechaFinal = new Date('Mar 8, 2024 23:50:15').getTime();
 //1709279400000
 
 let intervalo = setInterval(function(){
@@ -31,8 +33,98 @@ let intervalo = setInterval(function(){
     //cuando llegue a 0
     if(distancia <= 0){
         clearInterval(intervalo);
-        $panelFinal.style.transform = 'translateY(0)'
+        $body.style.overflowY = "auto"; 
+        $panelFinal.style.transform = 'translateY(0)';
+        $contenedor_historia.style.transform = 'translateY(0)';
+        $espacio_3.style.transform = 'translateY(0)';
+
     }
 }, 1000)
 
- 
+function scrollToSection() {
+    var seccionDestino = document.getElementById("contenedor_historia");
+    seccionDestino.scrollIntoView({ behavior: "smooth" });
+  }
+
+
+  document.addEventListener("DOMContentLoaded", function () {
+    let currentIndex = 0;
+    const saludos = document.querySelector(".saludos");
+    const totalItems = document.querySelectorAll(".saludos-item").length;
+  
+    function mostrarSiguiente() {
+      currentIndex = (currentIndex + 1) % totalItems;
+      actualizarCarrusel();
+    }
+  
+    function mostrarAnterior() {
+      currentIndex = (currentIndex - 1 + totalItems) % totalItems;
+      actualizarCarrusel();
+    }
+  
+    function actualizarCarrusel() {
+      const translateValue = -currentIndex * 100 + "%";
+      saludos.style.transform = "translateX(" + translateValue + ")";
+    }
+  
+    setInterval(mostrarSiguiente, 3000); // Cambia la imagen cada 3 segundos
+  });
+
+  function reproducirCancion() {
+    var pausa = document.getElementById("pausa");
+    var audio = document.getElementById("miAudio");
+  
+    if (audio.paused) {
+      audio.play();
+      pausa.src ="./fotos/Objetos/pausa.png";
+    } else {
+      audio.pause();
+      pausa.src = "./fotos/Objetos/play.png";
+
+    }
+  }
+
+/*
+document.querySelectorAll(".carousel").forEach((carousel) => {
+    const items = carousel.querySelectorAll(".carousel_item");
+    const buttonsHtml = Array.from(items, () => {
+        return `<span class="carousel_button"></span>`;
+    });
+
+    carousel.insertAdjacentHTML("beforeend", `
+        <div class="carousel_nav">
+            ${buttonsHtml.join("")}
+        </div>
+    `);
+
+    const buttons = carousel.querySelectorAll(".carousel_button");
+
+    buttons.forEach((button, i) => {
+        button.addEventListener("click", () => {
+            items.forEach((item) => item.classList.remove("carousel_item--selected"));
+            buttons.forEach((button) => button.classList.remove("carousel_button--selected"));
+
+            items[i].classList.add("carousel_item--selected");
+            button.classList.add("carousel_button--selected");
+        });
+
+        items[0].classList.add("carousel_item--selected");
+        buttons[0].classList.add("carousel_button--selected");
+    });
+
+    let currentIndex = 0;
+
+    // Función para mostrar el siguiente elemento del carousel
+    function showNext() {
+        items[currentIndex].classList.remove("carousel_item--selected");
+        buttons[currentIndex].classList.remove("carousel_button--selected");
+
+        currentIndex = (currentIndex + 1) % items.length;
+
+        items[currentIndex].classList.add("carousel_item--selected");
+        buttons[currentIndex].classList.add("carousel_button--selected");
+    }
+
+    // Configuración del intervalo para el desplazamiento automático
+    const autoScrollInterval = setInterval(showNext, 3000); // Cambia la duración en milisegundos según tus preferencias
+});*/
